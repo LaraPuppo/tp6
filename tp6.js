a94176d037fe7f212f01b455f110f578cbf37bf8
LaraPuppo <128708137+LaraPuppo@users.noreply.github.com>
2023-11-27 19:22:46 -0300

    Update tp6.js

--- tp6.js
+++ tp6.js
@@ -1,9 +1,16 @@
 let aventura;
 let pantallas = [];
+let fuente;
+let patroclo = [];
+let flecha;
 
-
-function preload(){
-  for ( let i = 0; i < 20; i++ ) {
+function preload() {
+  fuente = loadFont ('assets/greek-freak.ttf');
+  flecha = loadImage('assets/flecha.png');
+  for ( let i = 0; i < 3; i++ ) {
+    patroclo[i] = loadImage('assets/patroclo'+i+'.png');
+  }
+  for ( let i = 0; i < 21; i++ ) {
     pantallas[i] = loadImage("assets/pantalla"+ i +".png");
   }
 }
@@ -15,9 +22,13 @@ function setup() {
 
 function draw() {
   aventura.mostrar();
-  text("x:"+ mouseX + ",y:"+ mouseY, mouseX, mouseY);
+  //text("x:"+ mouseX + ",y:"+ mouseY, mouseX, mouseY);
 }
 
 function mousePressed() {
   aventura.interactuar();
+}
+
+function keyPressed() {
+  aventura.teclado(); 
 }
